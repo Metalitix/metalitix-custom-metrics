f3b854b75b7c4d353076a481ae4af3cdc9957161
KristianZ <2193492+KristianZ@users.noreply.github.com>
2024-02-08 15:28:16 -0800

    Updating styling of MetalitixClickListener

--- Javascript/MetalitixClickListener.js
+++ Javascript/MetalitixClickListener.js
@@ -1,20 +1,19 @@
-function addMetalitixClickListener(logger) {
-  function handleButtonClick(event) {
-      const eventName = event.target.getAttribute('mtx-event-name')
-      const chartName = event.target.getAttribute('mtx-chart-name')
+const addMetalitixClickListener = (logger) => {
+  const handleButtonClick = (event) => {
+    const eventName = event.target.getAttribute('mtx-event-name')
+    const chartName = event.target.getAttribute('mtx-chart-name')
 
-      if (!chartName) {
-          console.warn(`[MetalitixClickListener] An element with ${eventName} does not have a mtx-chart-name attribute! It was not recorded to Metalitix.`)
-      }
-      else if (logger.isRunning()) {
-          logger.logEvent(chartName, eventName)
-      }
+    if (!(chartName)) {
+      console.warn(`[MetalitixClickListener] An element with ${eventName} does not have a mtx-chart-name attribute! It was not recorded to Metalitix.`)
+    }
+
+    logger.logEvent(chartName, eventName)
   }
 
-  function setupEventListeners() {
-      document.querySelectorAll('[mtx-event-name]').forEach(element => {
-          element.addEventListener('click', handleButtonClick)
-      })
+  const setupEventListeners = () => {
+    document.querySelectorAll('[mtx-event-name]').forEach(element => {
+      element.addEventListener('click', handleButtonClick)
+    })
   }
 
   setupEventListeners()
@@ -22,14 +21,14 @@ function addMetalitixClickListener(logger) {
 
 if (typeof AFRAME !== 'undefined') {
   AFRAME.registerComponent('metalitix-click-listener', {
-      init: function () {
-          const loggerComponent = document.querySelector('a-scene').components['metalitix-logger']
-          if (loggerComponent && loggerComponent.logger) {
-            addMetalitixClickListener(loggerComponent.logger)
-          } else {
-              console.warn('[MetalitixClickListener] No metalitix-logger component found on the a-scene! Click events will not be recorded to Metalitix.')
-          }
+    init: function () {
+      const loggerComponent = document.querySelector('a-scene').components['metalitix-logger']
+      if (loggerComponent && loggerComponent.logger) {
+      addMetalitixClickListener(loggerComponent.logger)
+      } else {
+        console.warn('[MetalitixClickListener] No metalitix-logger component found on the a-scene! Click events will not be recorded to Metalitix.')
       }
+    }
   })
 }
 
