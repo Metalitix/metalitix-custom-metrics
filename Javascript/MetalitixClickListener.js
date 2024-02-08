56024d6433bd2a47b84145530b1dbbffc4668560
KristianZ <2193492+KristianZ@users.noreply.github.com>
2024-02-08 15:35:09 -0800

    Update attribute check for MetalitixClickListener

--- Javascript/MetalitixClickListener.js
+++ Javascript/MetalitixClickListener.js
@@ -2,17 +2,19 @@ const addMetalitixClickListener = (logger) => {
   const handleButtonClick = (event) => {
     const eventName = event.target.getAttribute('mtx-event-name')
     const chartName = event.target.getAttribute('mtx-chart-name')
-
-    if (!(chartName)) {
-      console.warn(`[MetalitixClickListener] An element with ${eventName} does not have a mtx-chart-name attribute! It was not recorded to Metalitix.`)
-    }
-
     logger.logEvent(chartName, eventName)
   }
 
   const setupEventListeners = () => {
-    document.querySelectorAll('[mtx-event-name]').forEach(element => {
-      element.addEventListener('click', handleButtonClick)
+    document.querySelectorAll('[mtx-chart-name],[mtx-event-name]').forEach(element => {
+      const hasChartName = element.hasAttribute('mtx-chart-name')
+      const hasEventName = element.hasAttribute('mtx-event-name')
+      
+      if (hasChartName && hasEventName) {
+        element.addEventListener('click', handleButtonClick)
+      } else {
+        console.warn('[MetalitixClickListener] Element must have both mtx-chart-name and mtx-event-name attributes to log events.', element)
+      }
     })
   }
 
