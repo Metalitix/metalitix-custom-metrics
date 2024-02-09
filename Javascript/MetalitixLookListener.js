dc141e2cf41d66033b8d42ee4d41f620bb0b4de6
KristianZ <2193492+KristianZ@users.noreply.github.com>
2024-02-09 12:09:41 -0800

    Fix MetalitixLookListener to work with a-entity

--- Javascript/MetalitixLookListener.js
+++ Javascript/MetalitixLookListener.js
@@ -1,21 +1,19 @@
-function addMetalitixLookListener(logger, scene, camera) {
+const addMetalitixLookListener = (logger, scene, camera, aframe=false) => {
   const raycaster = new THREE.Raycaster()
 
   scene.traverse(child => {
-    if (child.object3D && child.object3D.userData.mtxLookName) {
-      logger.setAttribute(child.object3D.userData.mtxLookName, false)
+    if (child.userData.mtxLookName) {
+      logger.setAttribute(child.userData.mtxLookName, false)
     }
   })
 
-  function checkLook() {
+  const checkLook = () => {
     raycaster.setFromCamera(new THREE.Vector2(0, 0), camera)
     const intersects = raycaster.intersectObjects(scene.children, true)
 
-    if (intersects.length > 0) {
-      const firstIntersectedObject = intersects[0].object
-      if (firstIntersectedObject.userData.mtxLookName) {
-        logger.setAttribute(firstIntersectedObject.userData.mtxLookName, true)
-      }
+    const firstIntersectedObject = (aframe) ? intersects[0]?.object?.parent?.parent : intersects[0]?.object
+    if (firstIntersectedObject && firstIntersectedObject.userData.mtxLookName) {
+      logger.setAttribute(firstIntersectedObject.userData.mtxLookName, true)
     }
   }
 
@@ -31,18 +29,19 @@ if (typeof AFRAME !== 'undefined') {
   AFRAME.registerComponent('metalitix-look-listener', {
     init: function () {
       const sceneEl = this.el.closestScene('a-scene')
-      const camera = sceneEl.camera
       const loggerComponent = sceneEl.components['metalitix-logger']
-      if (loggerComponent && loggerComponent.logger) {
-        sceneEl.querySelectorAll('[mtx-look-name]').forEach(el => {
-          el.object3D.userData.mtxLookName = el.getAttribute('mtx-look-name')
-        })
-        addMetalitixLookListener(loggerComponent.logger, sceneEl.object3D, camera)
-      } else {
+      
+      if(!(loggerComponent) || !(loggerComponent.logger)) {
         console.warn('[MetalitixLookListener] No metalitix-logger component found on the a-scene! Look events will not be recorded to Metalitix.')
+        return
       }
+
+      sceneEl.querySelectorAll('[mtx-look-name]').forEach(el => {
+        el.object3D.userData.mtxLookName = el.getAttribute('mtx-look-name')
+      })
+      addMetalitixLookListener(loggerComponent.logger, sceneEl.object3D, sceneEl.camera, true)
     }
   })
 }
-
-export { addMetalitixLookListener }
+  
+  export { addMetalitixLookListener }
